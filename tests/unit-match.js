3a7f662d493de09402143ac65ddb2cef7ff7124e
artyte <artyte@gmail.com>
2023-06-05 19:17:00 +0800

    add jest for test, unable to es6 imports

---
+++ tests/unit-match.js
@@ -0,0 +1,8 @@
+import Match from '../src/matchmaking/Match';
+
+describe('Match', () => {
+  it('should give team1', () => {
+    const match = new Match('abc', '123');
+    expect(match.getTeam1()).toBe('abc');
+  });
+});
