a3aa7484eca17fdc650fb98159a5a93bbc087431
artyte <artyte@gmail.com>
2023-06-08 19:56:49 +0800

    Load json data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { program as program } from 'commander';
+import fs from 'fs';
+import path from 'path';
 
 program
   .option('-f, --file <type>', 'use a json file as data')
@@ -8,11 +10,19 @@ program.parse(process.argv);
 
 const options = program.opts();
 
+var truePath = './data';
+
 if (options.file) {
   console.log(`Loading file data:${options.file}`);
+  truePath = options.file;
 } else {
   console.log(`Loading default json file`);
+  truePath = path.join('./data', 'sample-data.json');
 }
+
+const raw = fs.readFileSync(truePath);
+const data = JSON.parse(raw);
+console.log(data);
 
 if (options.match && ['1v1', '3v3', '5v5'].includes(options.match)) {
   console.log(`Using ${options.match} match format`);
