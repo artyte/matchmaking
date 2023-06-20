bdb6eaa3e30c15d27654e4ac3f4bb9497edc4deb
artyte <artyte@gmail.com>
2023-06-21 07:13:44 +0800

    missed comments

--- tests/unit-director.js
+++ tests/unit-director.js
@@ -3,7 +3,10 @@ import { expect } from '@jest/globals';
 
 describe('A Director', () => {
   it('should not initialize', () => {
+    // invalid parameter type
     expect(() => new Director('123')).toThrow();
+
+    // invalid parameter value
     expect(() => new Director(-1)).toThrow();
     expect(() => new Director(0)).toThrow();
   });
