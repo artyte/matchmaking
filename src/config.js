b58a63b26405f75537a22cfead5fbbc89962420a
artyte <artyte@gmail.com>
2023-06-22 05:09:10 +0800

    Stored frequently used variables in config file

---
+++ src/config.js
@@ -0,0 +1,34 @@
+import { program } from 'commander';
+
+program
+  .option('-f, --file <type>', 'use a json file as data')
+  .option('-t, --team <type>', 'select players per team: 3/5. default: 3');
+program.parse(process.argv);
+const options = program.opts();
+
+export const playersPerTeam = options.team ? Number(options.team) : 3;
+export const bins = [
+  [0, 1000, playersPerTeam],
+  [1000, 1300, playersPerTeam],
+  [1300, 1500, playersPerTeam],
+  [1500, 1700, playersPerTeam],
+  [1700, 2001, playersPerTeam],
+];
+export const serverNum = 1;
+export const filePath = options.file ? options.file : './data/sample-data.json';
+
+export const queueClients = 1000;
+export const matchClients = 5000;
+export const endAndPoll = 10000;
+
+console.log(playersPerTeam, bins, filePath);
+// export const makeBins = (playersPerTeam = 3) => {
+//   const bins = [
+//     [0, 1000, playersPerTeam],
+//     [1000, 1300, playersPerTeam],
+//     [1300, 1500, playersPerTeam],
+//     [1500, 1700, playersPerTeam],
+//     [1700, 2001, playersPerTeam],
+//   ];
+//   return bins;
+// };
--- src/index.js
+++ src/index.js
@@ -1,60 +1,33 @@
 /* eslint-disable no-console */
-import { program } from 'commander';
-import path from 'path';
 // import readline from 'readline';
-import process from 'node:process';
+// import process from 'node:process';
+import {
+  bins,
+  serverNum,
+  filePath,
+  queueClients,
+  matchClients,
+  endAndPoll,
+  playersPerTeam,
+} from './config';
 import Client from './matchmaking/Client';
 import Director from './matchmaking/Director';
 import MatchmakerImpl from './matchmaking/MatchmakerImpl';
 
-program
-  .option('-f, --file <type>', 'use a json file as data')
-  .option('-t, --team <type>', 'select players per team: 3/5. default: 3');
-
-program.parse(process.argv);
-
-const options = program.opts();
-
-let client;
-let truePath = './data';
-if (options.file) {
-  console.log(`Loading file data:${options.file}...`);
-  truePath = options.file;
-  client = new Client(truePath);
-} else {
-  console.log('Loading default json file...');
-  truePath = path.join('./data', 'sample-data.json');
-  client = new Client(truePath);
-}
-// client.getLoad();
+console.log(`Loading file data: ${filePath}...`);
+const client = new Client(filePath);
 
 console.log('Creating servers...');
-const director = new Director(1);
+const director = new Director(serverNum);
 
 console.log('Creating matchmaker...');
-let playersPerTeam;
-if (options.match && ['3', '5'].includes(options.match)) {
-  console.log(`Using ${options.match} match format`);
-  playersPerTeam = Number(options.match);
-} else {
-  console.log('Using default 3v3 match format');
-  playersPerTeam = 3;
-}
-
-const bins = [
-  [0, 1000, playersPerTeam],
-  [1000, 1300, playersPerTeam],
-  [1300, 1500, playersPerTeam],
-  [1500, 1700, playersPerTeam],
-  [1700, 2001, playersPerTeam],
-];
 const matchMaker = new MatchmakerImpl(bins);
-const activeMatches = [];
 
+const activeMatches = [];
 setInterval(() => {
   console.log('Client queueing a player...');
   client.queue(matchMaker);
-}, 1000);
+}, queueClients);
 
 setInterval(() => {
   console.log('Finding a match...');
@@ -66,8 +39,7 @@ setInterval(() => {
       activeMatches.push(match);
       console.log(match);
       console.log('Match starting...');
-    }
-    else {
+    } else {
       console.log('No available servers, going back to queue...');
       const team1 = match.getTeam1();
       const team2 = match.getTeam2();
@@ -75,7 +47,7 @@ setInterval(() => {
       team2.forEach((player) => matchMaker.backToMatchmaking(player));
     }
   }
-}, 5000);
+}, matchClients);
 
 setInterval(() => {
   const match = activeMatches.pop();
@@ -88,13 +60,13 @@ setInterval(() => {
       console.log(match);
       console.log('Match ended...');
     }
-  }  
+  }
 
   console.log('Updating players in client...');
   client.poll(director);
-}, 10000);
+}, endAndPoll);
 
 
-process.on('exit', (code) => {
-  console.log(`Shows stats here`);
-});
+// process.on('exit', (code) => {
+//   console.log(`Shows stats here`);
+// });
